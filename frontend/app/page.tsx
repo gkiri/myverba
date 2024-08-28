6a9c214b864ea2ab6346b6746c706592a16edea8
gkiri <hkkiri@gmail.com>
2024-08-28 16:58:58 +0200

    amazon aws backend chang

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -47,7 +47,8 @@ export default function Home() {
     try {
       const host = await detectHost();
       setAPIHost(host);
-      if (host === "" || host === "http://localhost:8000") {
+      //if (host === "" || host === "http://localhost:8000") {
+        if (host === "" || host === "http://3.83.67.48:8000") {
         try {
           const health_response = await fetch(host + "/api/health", {
             method: "GET",
