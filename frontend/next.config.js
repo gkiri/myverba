b181fc2fa87c09c01fa290d9ec3494a54e573939
gkiri <hkkiri@gmail.com>
2024-10-14 03:54:11 +0200

    nextjs build, static pages fffffffffffffffffoercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Disable server-side features when exporting
+    output: 'standalone',
     images: {
       unoptimized: true,
     },
-    // Remove rewrites configuration as it's not compatible with static export
 };
-
-// Set assetPrefix only in production/export mode
-if (process.env.NODE_ENV === 'production') {
-    nextConfig.assetPrefix = '/static';
-}
 
 module.exports = nextConfig;
