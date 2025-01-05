bb500d2ebc2a8fadfcdd571fdaf1278da076cdcd
gkiri <hkkiri@gmail.com>
2025-01-05 04:26:54 +0100

    prompts import structure change v2

--- goldenverba/server/api.py
+++ goldenverba/server/api.py
@@ -34,7 +34,7 @@ from goldenverba.server.util import get_config, set_config, setup_managers
 from goldenverba.components.types import Question # Add  Question model to types
 from pydantic import ValidationError
 #from goldenverba.server.prompts import get_prompt ,generate_prompt_chapter_user ,generate_prompt_chapter_user_query
-from goldenverba.server.prompts import prompts
+import goldenverba.server.prompts as prompts
 from goldenverba.server.supabase.supabase_client import supabase
 import asyncio
 load_dotenv()
