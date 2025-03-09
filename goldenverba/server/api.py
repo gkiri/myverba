1deb18eaaef8f6f89b8e9f21db9a1b6e772c1f34
gkiri <hkkiri@gmail.com>
2025-03-10 00:39:55 +0100

    backen ip cors

--- goldenverba/server/api.py
+++ goldenverba/server/api.py
@@ -203,6 +203,7 @@ origins = [
     "http://3.83.67.48:8000",
     "http://3.89.115.184:8000",
     "http://3.84.81.112:8000",
+    "http://3.86.59.21:8000",
     "https://gkiri-vercel-deploy.vercel.app",
     "https://*.vercel.app",
     "http://54.224.217.30:8000",
