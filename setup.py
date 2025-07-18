752841c9aa809fd9ed63309576530769edf923a9
gkiri <hkkiri@gmail.com>
2025-07-18 06:29:46 +0200

    baseline3 : openai verison

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         #"weaviate-client==3.23.1",
         "weaviate-client==4.6.4",
         "python-dotenv>=0.21.0,<0.22.0",
-        "openai==0.27.9",# current and old openai lib.if you dont need latest openrouter models support-this is good 
+        "openai==1.84.0",# current and old openai lib.if you dont need latest openrouter models support-this is good 
         #"openai==1.50.2",# New AsyncOpenai support needs this (I have already prepared Openroutergenerator for this for gemini with opentouer support)
         "wasabi==1.1.2",
         "fastapi==0.102.0",
